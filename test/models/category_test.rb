a8e195343ae68e6ca74d2472cd9deec4514ae9b6
devguykyle <kr556@nau.edu>
2024-01-03 17:48:54 -0500

    added more validations

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: 'testCategory')
+  end
+
+  test "category should be valid" do
     assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = ''
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category_too = Category.new(name: 'testCategory')
+    assert_not @category_too.valid?
   end
 end
